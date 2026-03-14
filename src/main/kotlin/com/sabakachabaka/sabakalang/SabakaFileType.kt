acda4489a44438bde3210a8f831e29b08c891fd4
sabaka-chabaka <gamekimg360@gmail.com>
2026-03-14 09:35:00 +0400

    icon hell

--- src/main/kotlin/com/sabakachabaka/sabakalang/SabakaFileType.kt
+++ src/main/kotlin/com/sabakachabaka/sabakalang/SabakaFileType.kt
@@ -8,7 +8,7 @@ class SabakaFileType private constructor() : LanguageFileType(SabakaLanguage) {
     override fun getName() = "SabakaLang"
     override fun getDescription() = "SabakaLang source file"
     override fun getDefaultExtension() = "sabaka"
-    override fun getIcon(): Icon = AllIcons.FileTypes.Text
+    override fun getIcon(): Icon = SabakaIcons.FILE
 
     companion object {
         @JvmField val INSTANCE = SabakaFileType()
---
+++ src/main/kotlin/com/sabakachabaka/sabakalang/SabakaIcons.kt
@@ -0,0 +1,10 @@
+package com.sabakachabaka.sabakalang
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object SabakaIcons {
+    @JvmField
+    val FILE: Icon = IconLoader.getIcon("/icons/file.png", SabakaIcons::class.java)
+
+}
